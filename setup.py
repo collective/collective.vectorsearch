58444128e03c1dd1e4f78cb86c013b3f6048d66d
Manabu TERADA <terada@cmscom.jp>
2026-02-05 17:36:36 +0900

    Python 3.9 removal changes

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Framework :: Plone :: 6.0",
         "Framework :: Plone :: 6.1",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
@@ -49,7 +48,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
