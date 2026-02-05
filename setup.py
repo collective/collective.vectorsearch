cd8843c5644c022a1fd3f09c0b605efc6f809ff2
Manabu TERADA <terada@cmscom.jp>
2026-02-05 17:26:38 +0900

    remove z3c.jbot

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
-        "z3c.jbot",
         "plone.api>=1.8.4",
         "plone.app.dexterity",
         "plone.app.registry",
